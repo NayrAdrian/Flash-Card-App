a1ec7dd7e510c8cf7ee47815cb9c42c780339251
Ryan Adrian <NayrAdrian24@gmail.com>
2024-07-12 22:02:00 +0800

    Added flip cards function (French to English)

--- main.py
+++ main.py
@@ -8,12 +8,28 @@ BACKGROUND_COLOR = "#B1DDC6"
 data = pd.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
 
+current_card = {}
+flip_timer = None
+
 
 def next_card():
+    global current_card, flip_timer
+    if flip_timer:
+        window.after_cancel(flip_timer)
+
     # Select a random word from the list
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(front_image, image=card_front_img)
+    # Schedule the flip to occur after 3 seconds
+    flip_timer = window.after(3000, flip_cards)
+
+
+def flip_cards():
+    canvas.itemconfig(front_image, image=card_back_img)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
 
 
 # Initialize the window
@@ -24,7 +40,9 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # Initialize the canvas
 canvas = Canvas(height=526, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+front_image = canvas.create_image(400, 263, image=card_front_img)
+back_image = canvas.create_image(400, 263, image=card_back_img)
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Text elements on the canvas
@@ -42,6 +60,8 @@ known_button = Button(image=check_image, highlightthickness=0, bg=BACKGROUND_COL
                       borderwidth=0, command=next_card)
 known_button.grid(row=1, column=1, pady=30)
 
+# Start with the first card
 next_card()
 
+# Start the Tkinter event loop
 window.mainloop()
