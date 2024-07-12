f607ceab2c2c94623cc59477cd9a05220cc5ef7e
Ryan Adrian <NayrAdrian24@gmail.com>
2024-07-12 17:32:40 +0800

    Updated UI

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flash Card App")
-window.config(bg=BACKGROUND_COLOR)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-frame = Frame(window, bg=BACKGROUND_COLOR)
-frame.grid(row=0, column=0, padx=50, pady=50)
-
-canvas = Canvas(frame, height=526, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
-front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=front_img)  # Center the image
+canvas = Canvas(height=526, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)  # Center the image
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Adding text inside the canvas
@@ -19,13 +16,14 @@ canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"), fill="b
 canvas.create_text(400, 263, text="Sample", font=("Arial", 60, "bold"), fill="black")
 
 # Buttons
-check_button = PhotoImage(file="images/right.png")
-cross_button = PhotoImage(file="images/wrong.png")
 
-wrong_button = Button(frame, image=cross_button, highlightthickness=0, bg=BACKGROUND_COLOR, activebackground=BACKGROUND_COLOR, borderwidth=0)
-wrong_button.grid(row=1, column=0, pady=20)
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, activebackground=BACKGROUND_COLOR, borderwidth=0)
+unknown_button.grid(row=1, column=0, pady=30)
 
-right_button = Button(frame, image=check_button, highlightthickness=0, bg=BACKGROUND_COLOR, activebackground=BACKGROUND_COLOR, borderwidth=0)
-right_button.grid(row=1, column=1, pady=20)
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, activebackground=BACKGROUND_COLOR, borderwidth=0)
+known_button.grid(row=1, column=1, pady=30)
 
 window.mainloop()
+
